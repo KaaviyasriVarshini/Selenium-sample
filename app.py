0940a7b8dc669f50abafc49785fcbb56208031f1
KaaviyasriVarshini <kaaviyasrivarshinir@gmail.com>
2022-06-05 21:47:20 +0530

    maximized chrome window

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from ipaddress import ip_address
 from selenium import webdriver
 
 PATH = "/home/sanjju/Downloads/chromedriver_linux64/chromedriver"
@@ -7,6 +6,8 @@ def startpy():
     driver.get("https://ifconfig.me/")
     ip_address = driver.find_element_by_xpath('//*[@id="ip_address"]')
     print(ip_address.text)
+    driver.maximize_window()
+
 
 
 startpy()
