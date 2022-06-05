80f9d4762a9e6698cb30fb625ff2a90d26830b99
KaaviyasriVarshini <kaaviyasrivarshinir@gmail.com>
2022-06-05 21:40:42 +0530

    ip address scraped

---
+++ app.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_address
+from selenium import webdriver
+
+PATH = "/home/sanjju/Downloads/chromedriver_linux64/chromedriver"
+driver = webdriver.Chrome(PATH)
+def startpy():
+    driver.get("https://ifconfig.me/")
+    ip_address = driver.find_element_by_xpath('//*[@id="ip_address"]')
+    print(ip_address.text)
+
+
+startpy()
