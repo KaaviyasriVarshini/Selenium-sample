167f0d9d83ac573eb7583bb2d670057a9f243c52
KaaviyasriVarshini <kaaviyasrivarshinir@gmail.com>
2022-06-25 18:59:23 +0530

    added csv file

--- harry_film.py
+++ harry_film.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import pandas as pd
 
 
 
@@ -8,21 +9,34 @@ driver = webdriver.Chrome(PATH)
 
 def startpy():
 
+    _HARRY_POTTER_ = []
+
     URL = "https://en.wikipedia.org/wiki/Harry_Potter_(film_series)"
 
     driver.get(URL)
     driver.maximize_window()
     time.sleep(5)
-    harry_potter = { }
+    
 
     for i in range(2, 10):
+        
+        harry_potter = {}
     
         year = driver.find_element_by_xpath(f'//*[@id="mw-content-text"]/div[1]/table[2]/tbody/tr[{i}]/td[1]')
         print(year.text)
         film = driver.find_element_by_xpath(f'//*[@id="mw-content-text"]/div[1]/table[2]/tbody/tr[{i}]/td[2]')
         print(film.text)
-        harry_potter[year.text] = film.text
-    print(harry_potter)
+
+        harry_potter["Year"] = [year.text]
+        harry_potter["Film"] = [film.text]
+        _HARRY_POTTER_.append(harry_potter)
+
+        df = pd.DataFrame.from_dict(_HARRY_POTTER_)
+        df.to_csv("._.Harry Potter._.csv", index = False, header = True)
+
+
+    #     harry_potter[year.text] = film.text
+    # print(harry_potter)
 
 
 
